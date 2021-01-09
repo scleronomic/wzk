c8d2a1a9d4d054c7b0c9e3f1eb6c32842658ab12
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-01-09 23:41:03 +0100

    added ssh functionality

--- wzk/ssh.py
+++ wzk/ssh.py
@@ -5,13 +5,10 @@ import subprocess
 def execute_via_ssh(remote_client, cmd):
     # ssh -tt for short to force pseudo-tty allocation even if stdin isn't a terminal.
     # https://stackoverflow.com/questions/7114990/pseudo-terminal-will-not-be-allocated-because-stdin-is-not-a-terminal
-    ssh_process = subprocess.Popen(f"ssh {remote_client} -tt",
-                                   stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                                   universal_newlines=True, shell=True)
-    ssh_process.stdin.write(cmd + '\n')
-    ssh_process.stdin.close()
-    print(ssh_process.stdout.read())
-
+    process = subprocess.Popen(f"ssh {remote_client} -tt; {cmd}", stdout=subprocess.PIPE,  shell=True)
+    process = subprocess.Popen(f"{cmd}; {cmd}", stdout=subprocess.PIPE,  shell=True)
+    stdout = process.communicate()[0].strip()
+    return proc_stdout
 
 def test_execute_via_ssh():
     execute_via_ssh('rmc-lx0144', 'ls')
