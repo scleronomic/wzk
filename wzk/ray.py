eb85c28e93c0800df115eb92788a1caa2e44e1cc
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-01-11 14:12:31 +0100

    correct ray

--- wzk/ray.py
+++ wzk/ray.py
@@ -7,7 +7,7 @@ import fire
 import numpy as np
 
 from wzk.ssh import ssh_cmd
-from wzk.dicts_lists_tuples import safe_squeeze
+from wzk.dicts_lists_tuples import safe_squeeze, atleast_list
 
 
 __default_nodes = ['rmc-lx0062', 'rmc-lx0144', 'rmc-lx0140', 'rmc-lx0271',
@@ -42,6 +42,7 @@ def start_ray_cluster(head=None, nodes=None, verbose=2):
     address = address[address.find('=')+1:]
     password = password[password.find('=')+1:]
 
+    nodes = np.setdiff1d(atleast_list(nodes), [head])
     start_node_cmd = f"ray start --address='{address}' --redis-password='{password}'"
     for node in nodes:
         stdout = ssh_cmd(host=node, cmd=start_node_cmd)
@@ -52,6 +53,27 @@ def start_ray_cluster(head=None, nodes=None, verbose=2):
     np.save(os.path.abspath(os.path.dirname(__file__)) + '/' + 'ray_start.text', log)
 
 
+def stop_ray_cluster(nodes=None, verbose=1):
+    if nodes is None:
+        nodes = __default_nodes
+
+    if verbose > 0:
+        print('Stop Ray-Cluster')
+        print('Nodes: ', *nodes)
+
+    for node in atleast_list(nodes):
+        stdout = ssh_cmd(host=node, cmd='ray stop --force')
+        if verbose > 1:
+            print(node, ':', stdout)
+
+def ray_main(mode='start', nodes=None, head=None, verbose=1):
+    if mode == 'start':
+        start_ray_cluster(head=head, nodes=nodes, verbose=verbose)
+    elif mode == 'stop':
+        stop_ray_cluster(nodes=nodes, verbose=verbose)
+    else:
+        raise ValueError
+
 
 if __name__ == '__main__':
-    fire.Fire(start_ray_cluster)
+    fire.Fire(ray_main)
