0c3c6e764586b2280a954574ebda0c61155d666f
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2021-06-29 14:29:33 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
                       'scikit-image',
 
                       'matplotlib',
-                      'pyvista'
+                      'pyvista',
                       'setuptools',
                       'msgpack'],
     classifiers=[
