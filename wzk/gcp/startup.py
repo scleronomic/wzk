1f7bfc59ba7a74199402297c903efc37c8685c0f
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-02-02 11:48:33 +0100

    fix bugs in sql

--- wzk/gcp/startup.py
+++ wzk/gcp/startup.py
@@ -3,10 +3,12 @@
 def make_startup_file(user, bash_file):
     s = f"#!/bin/bash\n" \
         f"source ~/.bashrc\n" \
-        f"sudo chmod 777 -R /home/{user}/src/\n" \
-        f"sudo -H -u {user} python /home/{user}/src/wzk/wzk/git2.py\n" \
+        f"sudo chmod 777 -R /home/{user}/src\n" \
         f"sudo -H -u {user} tmux new-session -d -s main\n" \
         f"sudo -H -u {user} tmux send 'source {bash_file}' C-m\n" \
+        f"sudo -H -u {user} tmux send 'cd /home/{user}' C-m\n" \
         f"sudo -H -u {user} tmux -2  attach-session -t main\n"
     return s
 
+
+ # f"python /home/{user}/src/wzk/wzk/git2.py\n" \         # f"sudo chmod 777 -R /home/{user}/src/\n" \
