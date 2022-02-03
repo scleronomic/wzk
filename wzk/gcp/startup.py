1e47547cb571bfaebb8fb0a89c9ef98628e352c6
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-02-03 23:42:28 +0100

    updates to sql

--- wzk/gcp/startup.py
+++ wzk/gcp/startup.py
@@ -4,8 +4,8 @@ def make_startup_file(user, bash_file):
     s = f"#!/bin/bash\n" \
         f"source ~/.bashrc\n" \
         f"sudo -H -u {user} sudo chmod 777 -R /home/{user}/src\n" \
-        f"python /home/{user}/src/wzk/wzk/git2.py\n" \
         f"sudo -H -u {user} tmux new-session -d -s main\n" \
+        f"sudo -H -u {user} tmux send 'python /home/{user}/src/wzk/wzk/git2.py' C-m\n" \
         f"sudo -H -u {user} tmux send 'source {bash_file}' C-m\n" \
         f"sudo -H -u {user} tmux send 'cd /home/{user}' C-m\n" \
         f"sudo -H -u {user} tmux -2  attach-session -t main\n"
