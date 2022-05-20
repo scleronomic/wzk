f962751deb20341cc6039239395ca6feb7cb7d58
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-05-20 16:48:25 +0200

    dtypes bool

--- wzk/dtypes.py
+++ wzk/dtypes.py
@@ -10,13 +10,30 @@ __str2np = {'f': np.float64,
             'i32': np.int32,
             'i16': np.int16,
             'i8': np.int8,
-            'b': np.bool,
+            'b': bool,
             'cmp': object,  # byte strings can have varying length and otherwise there is a fuck up
             'zlib': object,  # TODO does this make sense to add?
             't': str,
             'txt': str,
             'str': str,
             }
+
+# __str2sql = {'f': np.float64,  # TODO
+#              'f64': np.float64,
+#              'f32': np.float32,
+#              'f16': np.float16,
+#              'i': np.int32,
+#              'i64': np.int64,
+#              'i32': np.int32,
+#              'i16': np.int16,
+#              'i8': np.int8,
+#              'b': np.bool,
+#              'cmp': object,  # byte strings can have varying length and otherwise there is a fuck up
+#              # 'zlib': object,  # TODO does this make sense to add?
+#              't': str,
+#              'txt': str,
+#              'str': str,
+#              }
 
 
 def str2np(s: str, strip: bool = True):
@@ -33,7 +50,7 @@ def astype(a, s):
     return a.astype(str2np(s))
 
 
-c2np = {bool: np.bool_,
+c2np = {bool: bool,
         str: np.str_,
         int: np.integer,
         float: np.floating}
