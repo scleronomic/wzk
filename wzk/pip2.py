77714079b2b2bd30963c2c6737aeda0c39432052
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-07-04 11:58:10 +0200

    stable 1.2

--- wzk/pip2.py
+++ wzk/pip2.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import pkg_resources
 from subprocess import call
 
@@ -5,3 +8,22 @@ from subprocess import call
 def upgrade_all():
     packages = [dist.project_name for dist in pkg_resources.working_set]
     call("pip install --upgrade " + ' '.join(packages), shell=True)
+
+
+def get_python_version():
+    return sys.version_info[0], sys.version_info[1]
+
+
+def get():
+    pass
+
+
+def get_conda_site_packages():
+    pythonXY = f"python{sys.version_info[0]}{sys.version_info[1]}"
+    cs = os.environ.get('CONDA_PREFIX') + f"/lib/{pythonXY}/site-packages/"
+    return cs
+
+
+def get_conda_include():
+    ci = os.environ.get("CONDA_PREFIX") + "/include/"
+    return ci
