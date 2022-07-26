42330cccc947cb140bb97250ef60e70ea40fe278
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2022-07-26 18:22:11 +0200

    add bisect method

--- wzk/math2.py
+++ wzk/math2.py
@@ -326,6 +326,35 @@ def d_rosenbrock2d(xy, a=1, b=100):
     return np.concatenate((dx[..., np.newaxis], dy[..., np.newaxis]), axis=-1)
 
 
+def bisection(f, a, b, tol):
+    """
+    https://pythonnumericalmethods.berkeley.edu/notebooks/chapter19.03-Bisection-Method.html
+    Approximates a root of f bounded by a and b to within tolerance
+    | f(m) | < tol with m the midpoint between a and b.
+
+    Recursive implementation
+    """
+    # check if a and b bound a root
+    if np.sign(f(a)) == np.sign(f(b)):
+        raise Exception("The scalars a and b do not bound a root")
+
+    # get midpoint
+    m = (a + b) / 2
+
+    if np.abs(f(m)) < tol:
+        # stopping condition, report m as root
+        return m
+
+    elif np.sign(f(a)) == np.sign(f(m)):
+        # case where m is an improvement on a
+        # Make recursive call with a = m
+        return bisection(f, m, b, tol)
+    elif np.sign(f(b)) == np.sign(f(m)):
+        # case where m is an improvement on b
+        # Make recursive call with b = m
+        return bisection(f, a, m, tol)
+
+
 # Derivative
 def numeric_derivative(fun, x, eps=1e-5, axis=-1, mode='central',
                        diff=None,
