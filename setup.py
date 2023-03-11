d81f2550b2c333c79fe9e8365538f20394c5febf
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2023-03-11 23:37:12 +0100

    stable1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wzk",
-    version="0.1.6",
+    version="0.1.7",
     author="Johannes Tenhumberg",
     author_email="johannes.tenhumberg@gmail.com",
     description="WZK - WerkZeugKasten - Collection of different Python Tools",
